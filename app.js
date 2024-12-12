86a1fbf91c1f939b96e1ae6da33b692b1804181b
Varun Thapliyal <varunthapliyal70@gmail.com>
2024-12-12 13:12:40 +0530

    Bugs fixed

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 require("dotenv").config();
-const { PORT } = process.env;
 const mongoose = require("mongoose");
 const cors = require("cors");
+
 const formRoutes = require("./routes/formRoutes");
 const responseRoutes = require("./routes/responseRoutes");
 
@@ -14,6 +14,10 @@ const options = {
 app.use(express.json());
 app.use(cors(options));
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.use("/api/v1/forms", formRoutes);
 app.use("/api/v1/responses", responseRoutes);
 
@@ -21,10 +25,9 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
   })
   .catch((err) => {
-    console.log(err);
+    console.log("MongoDB Connection Error: ", err);
   });
+
+module.exports = app;
